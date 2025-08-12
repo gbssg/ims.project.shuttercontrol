3deb3b06e82425201d19f069d2c1a55108ffdee8
Bastian Lehmann <bastian.lehmann@quickline.ch>
2025-08-12 15:50:32 +0200

    timeout for Wifi

--- plattformIO/shuttercontrol_v3/lib/NetworkServer/NetworkServer.cpp
+++ plattformIO/shuttercontrol_v3/lib/NetworkServer/NetworkServer.cpp
@@ -1,17 +1,35 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WebServer.h>
 #include "NetworkServer.h"
 #include "secrets.h"
 
+int wifiTryCount;
+
 void connectWifi()
 {
+    wifiTryCount = 0;
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.print("Connecting to WiFi...");
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED && wifiTryCount < 20) {
         delay(500);
         Serial.print(".");
+        wifiTryCount++;
+    }
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        Serial.println("");
+        Serial.print("Wifi Connected");
+        Serial.println("");
+    }
+    else
+    {
+        Serial.println("");
+        Serial.print("Connection Failed");
+        Serial.println("");
     }
 }
+
 void handleClient()
 {
     
