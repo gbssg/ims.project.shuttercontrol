4a84c2f18b706d4d9f7c19ec981723dbdff26a0c
Bastian Lehmann <bastian.lehmann@quickline.ch>
2025-09-09 15:19:05 +0200

    started functions for Motor

--- plattformIO/shuttercontrol_v4/lib/Motor/Motor.cpp
+++ plattformIO/shuttercontrol_v4/lib/Motor/Motor.cpp
@@ -1,3 +1,26 @@
 #include "Motor.h"
 
- 
+void tMotor::setup()
+{
+
+}
+
+void tMotor::up()
+{
+
+}
+
+void tMotor::down()
+{
+
+}
+
+void tMotor::stop()
+{
+
+}
+
+uintptr_t tMotor::getState()
+{
+
+}
--- plattformIO/shuttercontrol_v4/lib/Motor/Motor.h
+++ plattformIO/shuttercontrol_v4/lib/Motor/Motor.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <Arduino.h>
+#include <SimpleSoftTimer.h>
+using namespace HolisticSolutions;
+
+typedef struct sMotor 
+{
+    void setup();
+    void up();
+    void down();
+    void stop();
+    void run();
+    uintptr_t getState();
+    SimpleSoftTimer* sstTimeout;
+}tMotor;
