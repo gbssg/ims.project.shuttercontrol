34748c9492bf80ca7d46d257412bfe0207c94981
Bastian Lehmann <bastian.lehmann@quickline.ch>
2025-09-09 15:18:34 +0200

    starting SimpleStateProcessor

--- plattformIO/shuttercontrol_v4/lib/Control/Control.cpp
+++ plattformIO/shuttercontrol_v4/lib/Control/Control.cpp
@@ -1,15 +1,15 @@
 #include "Control.h"
 
 SSP_STATE_HANDLER(ControlStateUnknown);
-SSP_STATE_HANDLER(ControlStateIdle);
-SSP_STATE_HANDLER(ControlStateGoingUp);
-SSP_STATE_HANDLER(ControlStateGoingDown);
+SSP_STATE_HANDLER(ControlStateStop);
+SSP_STATE_HANDLER(ControlStateUp);
+SSP_STATE_HANDLER(ControlStateDown);
 
 static const tSSP_State ControlStateMachine[] = {
     SSP_STATE_DESCRIBE("Unknown", ControlStateUnknown),
-    SSP_STATE_DESCRIBE("Idle", ControlStateIdle),
-    SSP_STATE_DESCRIBE("goingUp", ControlStateGoingUp),
-    SSP_STATE_DESCRIBE("goingDown", ControlStateGoingDown),
+    SSP_STATE_DESCRIBE("Stop", ControlStateStop),
+    SSP_STATE_DESCRIBE("Up", ControlStateUp),
+    SSP_STATE_DESCRIBE("Down", ControlStateDown),
 
     SSP_STATE_LAST()};
 
@@ -37,3 +37,79 @@ uintptr_t tControl::getState()
 tControl* control1 = new tControl();
 tControl* control2 = new tControl();
 
+SSP_STATE_HANDLER(ControlStateUnknown)
+{
+    switch (reason)
+    {
+    case SSP_REASON_ENTER:
+        fsm-> NextStateSet(CONTROL_ST_IDLE);
+        Serial.println("Unknown");
+        buttonUp->LEDon();
+        buttonDown->LEDon();
+        break;
+    case SSP_REASON_DO:
+        break;
+    case SSP_REASON_EXIT:
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+SSP_STATE_HANDLER(ControlStateStop)
+{
+    switch (reason)
+    {
+    case SSP_REASON_ENTER:
+        fsm-> NextStateSet(CONTROL_ST_IDLE);
+        Serial.println("Unknown");
+
+        break;
+    case SSP_REASON_DO:
+        break;
+    case SSP_REASON_EXIT:
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+SSP_STATE_HANDLER(ControlStateUp)
+{
+    switch (reason)
+    {
+    case SSP_REASON_ENTER:
+        fsm-> NextStateSet(CONTROL_ST_IDLE);
+        Serial.println("Unknown");
+
+        break;
+    case SSP_REASON_DO:
+        break;
+    case SSP_REASON_EXIT:
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+SSP_STATE_HANDLER(ControlStateDown)
+{
+    switch (reason)
+    {
+    case SSP_REASON_ENTER:
+        fsm-> NextStateSet(CONTROL_ST_IDLE);
+        Serial.println("Unknown");
+
+        break;
+    case SSP_REASON_DO:
+        break;
+    case SSP_REASON_EXIT:
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
