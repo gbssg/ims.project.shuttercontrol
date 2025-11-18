0d86c87a065599ee6e0d8ad277bf1bbfc8a29225
Bastian Lehmann <bastian.lehmann@quickline.ch>
2025-11-18 09:31:54 +0100

    add structure to control class

--- plattformIO/shuttercontrol_v4/src/Control/Control.h
+++ plattformIO/shuttercontrol_v4/src/Control/Control.h
@@ -1,1 +1,9 @@
 #pragma once
+
+typedef struct sControl tControl;
+
+tControl *Control_create();
+void Control_destroy(tControl *me);
+
+void Control_init(tControl *me);
+void Control_deinit(tControl *me);
