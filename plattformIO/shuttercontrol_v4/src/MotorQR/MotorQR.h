91906e6c73c921be39d344eb5e9353e6429b48c1
Bastian Lehmann <bastian.lehmann@quickline.ch>
2025-11-18 16:20:43 +0100

    add motor ssp in header file

--- plattformIO/shuttercontrol_v4/src/MotorQR/MotorQR.h
+++ plattformIO/shuttercontrol_v4/src/MotorQR/MotorQR.h
@@ -1,15 +1,48 @@
 #pragma once
 #include "IMotor/IMotor.h"
+#include <SimpleStateProcessor.h>
+#include <SimpleSoftTimer.h>
+using namespace HolisticSolutions;
 
 typedef struct sMotorQR tMotorQR;
 
-tMotorQR *MotorQR_create();
+
+
+tMotorQR *MotorQR_create(int channelNr);
 void MotorQR_destroy(tMotorQR *me);
 
-void MotorQR_init(tMotorQR *me);
+void MotorQR_init(tMotorQR *me, int channelNr);
 void MotorQR_deinit(tMotorQR *me);
+
+typedef struct sChannelDescriptor
+{
+    uint8_t relayUp;
+    uint8_t relayDown;
+}tChannelDescriptor;
+
+const tChannelDescriptor channels[] = {
+    {.relayUp = 1, .relayDown = 2},
+    {.relayUp = 3, .relayDown = 4}
+};
+
+typedef enum eMotorState
+{
+  MOTOR_ST_UNKNOWN = 0,
+  MOTOR_ST_IDLE,
+  MOTOR_ST_IDLEUP,
+  MOTOR_ST_IDLEDOWN,
+  MOTOR_ST_GOINGUP,
+  MOTOR_ST_GOINGDOWN,
+  MOTOR_ST_WAITUP,
+  MOTOR_ST_WAITDOWN,
+
+  MOTOR_ST_COUNT
+} tMotorState;
 
 struct sMotorQR
 {
     tIMotor motor;
+    const tChannelDescriptor *channel;
+    SimpleStateProcessor *ssp;
+    SimpleSoftTimer *timer;
 };
