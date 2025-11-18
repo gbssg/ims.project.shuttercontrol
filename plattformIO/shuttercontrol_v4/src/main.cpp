d14805af91e793653862a725cc6c38944bd8a6e1
Bastian Lehmann <bastian.lehmann@quickline.ch>
2025-11-18 16:21:30 +0100

    temp test code

--- plattformIO/shuttercontrol_v4/src/main.cpp
+++ plattformIO/shuttercontrol_v4/src/main.cpp
@@ -1,13 +1,22 @@
 #include <Arduino.h>
-#include <SimpleStateProcessor.h>
+#include "Control/Control.h"
+#include "MotorQR/MotorQR.h"
+#include "Scheduler/Scheduler.h"
+
+tControl *control;
+tMotorQR *motor;
 
 void setup()
 {
     Serial.begin(115200);
+    control = Control_create(0);
+    motor = MotorQR_create(0);
     // Wire.begin();
+    control->run(control);
+    Serial.println(control->button->addrUp);
 }
 
 void loop() 
 {
-    
+    control->run(control);
 }
