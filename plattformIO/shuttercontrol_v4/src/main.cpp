35e048eb2441500f40953bf43b76136af57a16c7
Bastian Lehmann <bastian.lehmann@quickline.ch>
2025-11-20 09:19:34 +0100

    explain wire begin

--- plattformIO/shuttercontrol_v4/src/main.cpp
+++ plattformIO/shuttercontrol_v4/src/main.cpp
@@ -8,10 +8,11 @@ tMotorQR *motor;
 
 void setup()
 {
+    // Needed When using Qwiic Components
+    // Wire.begin();
     Serial.begin(115200);
     control = Control_create(0);
     motor = MotorQR_create(0);
-    // Wire.begin();
     control->run(control);
     Serial.println(control->button->addrUp);
 }
