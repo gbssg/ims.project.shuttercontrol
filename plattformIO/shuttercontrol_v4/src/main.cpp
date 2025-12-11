e87875eb1a339c22e34b822a92a9597f71d03488
Bastian Lehmann <bastian.lehmann@quickline.ch>
2025-12-11 12:08:18 +0100

    main without scheduler

--- plattformIO/shuttercontrol_v4/src/main.cpp
+++ plattformIO/shuttercontrol_v4/src/main.cpp
@@ -4,20 +4,30 @@
 #include "Scheduler/Scheduler.h"
 
 tControl *control;
+tControl *control1;
 tMotorQR *motor;
+tMotorQR *motor1;
 
 void setup()
 {
     // Needed When using Qwiic Components
-    // Wire.begin();
+    Wire.begin();
     Serial.begin(115200);
-    control = Control_create(0);
     motor = MotorQR_create(0);
-    control->run(control);
+    motor1 = MotorQR_create(1);
+    
+    control = Control_create(0, &motor->motor);
+    control1 = Control_create(1, &motor1->motor);
     Serial.println(control->button->addrUp);
+    Serial.print("Debounce Time: ");
+    Serial.println(control->buttonUp->getDebounceTime());
 }
 
 void loop() 
 {
-    control->run(control);
+    // control->run(control);
+    control->ssp->run();
+    control1->ssp->run();
+    motor1->ssp->run();
+    motor->ssp->run();
 }
