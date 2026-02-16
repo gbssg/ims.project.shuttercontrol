9eb348bd3c5a504dbae5e97470435d93dd267da2
Bastian Lehmann <bastian.lehmann@quickline.ch>
2026-02-16 18:33:28 +0100

    add buttons for presets

--- website-test/main.js
+++ website-test/main.js
@@ -1,31 +1,107 @@
-const ip = "http://192.168.150.41/";
-document.getElementById("up_button").addEventListener("click",()=>{
-    console.log("Up");
-    fetch(`${ip}up`, {
-  mode: 'cors',
-  headers: {
-    'Access-Control-Allow-Origin':'*'
-  }
-});
-});
-
-document.getElementById("down_button").addEventListener("click",()=>{
-    console.log("Down");
-    fetch(`${ip}down`, {
-  mode: 'cors',
-  headers: {
-    'Access-Control-Allow-Origin':'*'
-  }
-});
-});
+const ip = "http://esp32-4cb7e0.cl04.ch/";
 
 
-document.getElementById("stop_button").addEventListener("click",()=>{
-    console.log("Stop");
-    fetch(`${ip}stop`, {
-  mode: 'cors',
-  headers: {
-    'Access-Control-Allow-Origin':'*'
-  }
+document.getElementById("up_button").addEventListener("click", () => {
+  console.log("Up");
+  fetch(
+    `${ip}motor?id=1&cmd=up`,{}
+  );
 });
+
+document.getElementById("down_button").addEventListener("click", () => {
+  console.log("Down");
+  fetch(
+    `${ip}motor?id=1&cmd=down`,{}
+  );
 });
+
+document.getElementById("stop_button").addEventListener("click", () => {
+  console.log("Stop");
+  fetch(
+    `${ip}motor?id=1&cmd=stop`,{}
+  );
+});
+
+document.getElementById("up_button_2").addEventListener("click", () => {
+  console.log("Up");
+  fetch(
+    `${ip}motor?id=2&cmd=up`,{}
+  );
+});
+
+document.getElementById("down_button_2").addEventListener("click", () => {
+  console.log("Down");
+  fetch(
+    `${ip}motor?id=2&cmd=down`,{}
+  );
+});
+
+document.getElementById("stop_button_2").addEventListener("click", () => {
+  console.log("Stop");
+  fetch(
+    `${ip}motor?id=2&cmd=stop`,{}
+  );
+});
+
+document.getElementById("preset1").addEventListener("click", () => {
+  fetch(`${ip}motor?id=1&cmd=down`);
+  fetch(`${ip}motor?id=2&cmd=down`);
+  console.log("preset1");
+  setTimeout(() => {
+    console.log("preset1");
+    fetch(
+      `${ip}motor?id=1&cmd=up`,{}
+    );
+    fetch(
+      `${ip}motor?id=2&cmd=up`,{}
+    );
+
+    setTimeout(() => {
+      console.log("preset1");
+      fetch(
+        `${ip}motor?id=1&cmd=stop`,{}
+      );
+      fetch(
+        `${ip}motor?id=2&cmd=stop`,{}
+      );
+    }, 2000);
+
+  }, 43000);
+});
+
+document.getElementById("preset2").addEventListener("click", () => {
+  fetch(`${ip}motor?id=1&cmd=down`);
+  fetch(`${ip}motor?id=2&cmd=down`);
+  console.log("preset1");
+  setTimeout(() => {
+    fetch(
+      `${ip}motor?id=1&cmd=stop`,{}
+    );
+    fetch(
+      `${ip}motor?id=2&cmd=stop`,{}
+    );
+  }, 43000);
+});
+
+document.getElementById("preset3").addEventListener("click", () => {
+  fetch(`${ip}motor?id=1&cmd=up`);
+  fetch(`${ip}motor?id=2&cmd=up`);
+  console.log("preset1");
+  setTimeout(() => {
+    fetch(
+      `${ip}motor?id=1&cmd=stop`,{}
+    );
+    fetch(
+      `${ip}motor?id=2&cmd=stop`,{}
+    );
+  }, 43000);
+});
+
+
+document.getElementById("preset4").addEventListener("click", () => {
+  fetch(`${ip}motor?id=1&cmd=stop`);
+  fetch(`${ip}motor?id=2&cmd=stop`);
+});
+
+
+
